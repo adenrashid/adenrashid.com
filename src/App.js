dc66b3f25ba1e80195a0b073ad8de7f255077245
aden <adenrashid@hotmail.com>
2020-12-14 16:44:52 +1100

    updated resume again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Routes from './Routes';
 import Nav from './Nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
@@ -16,17 +15,8 @@ function App() {
           <Route path='/about' component={About}/>
           <Route path='/projects' component={Projects}/>
           <Route path='/contact' component={Contact}/>
-          <Route path='/github' component={() => { 
-     window.location.href = 'https://github.com/adenrashid'; 
-     return null;
-}}/>
-          <Route path='/linkedin' component={() => { 
-     window.location.href = 'https://www.linkedin.com/in/adenrashid/'; 
-     return null;
-}}/>
         </Switch>
       </Router>
-      {/* <Routes /> */}
     </div>
   );
 }
